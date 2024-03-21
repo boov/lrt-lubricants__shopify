e97419aa92402372d97d857a906b869361c491fe
Matthew Booth <matthew@boov.co.uk>
2024-03-21 10:58:01 +0000

    REMOVED: color_scheme functionality
    REMOVED: Lots of other shit

--- bundle.mjs
+++ bundle.mjs
@@ -1,31 +1,52 @@
-import { transform, bundle } from "lightningcss";
+import chokidar from "chokidar";
 import { writeFileSync } from "fs";
+import { bundle, transform } from "lightningcss";
 import { env } from "process";
+import * as sass from "sass";
 
+// Dawn CSS
 try {
-  const start = performance.now();
-
-  // const { css: compiled } = sass.compile("_src/_assets/styles/main.scss", {
-  //   loadPaths: ["./_src/_assets/styles", "./node_modules"]
-  // });
-
   const { code: bundled } = bundle({
-    filename: "./src/styles/all.css",
+    filename: "./src/styles/dawn.css",
     minify: false
   });
 
-  if (env === "production" || true) {
-    const { code: transformed } = transform({
-      code: bundled,
-      minify: true
-    });
-    writeFileSync("./assets/main.css", transformed);
-  } else {
-    // writeFileSync("./assets/main.css", bundled);
-  }
+  const { code: transformed } = transform({
+    code: bundled,
+    minify: true
+  });
 
-  const end = performance.now();
-  console.log(`Compiled all CSS in ${Math.round(end - start)}ms.`);
+  writeFileSync("./assets/dawn.css", transformed);
+
+  console.log("Compiled all Dawn CSS");
 } catch (err) {
   console.error(err);
 }
+
+// LRT SCSS
+chokidar.watch("./src/styles/**/*.scss").on("change", async path => {
+  console.log(`File ${path} has been changed, recompiling...`);
+
+  try {
+    const start = performance.now();
+
+    const { css: compiled } = sass.compile("./src/styles/main.scss", {
+      loadPaths: ["./src/styles/", "./node_modules"]
+    });
+
+    if (env === "production") {
+      const { code: transformed } = transform({
+        code: compiled,
+        minify: true
+      });
+      writeFileSync("./assets/main.css", transformed);
+    } else {
+      writeFileSync("./assets/main.css", compiled);
+    }
+
+    const end = performance.now();
+    console.log(`Compiled all SCSS in ${Math.round(end - start)}ms.`);
+  } catch (err) {
+    console.error(err);
+  }
+});
